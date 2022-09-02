e6f629a8d5e7b88860b7e25e37dbd2374aa373fc
yexian <guanpengpeng@yeah.net>
2022-09-02 10:03:25 +0800

    生成游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,28 @@
-a22 = 2
-a = 3
-ab2 = 3
-ab33434 = 3
-afb2 = 3
-print(a + ab2)
+import sys
+import pygame
 
 
-def ceshi():
-    print("测试")
+class AlienInvasion:
+    # 管理游戏资源和行为的类
+    def __init__(self):
+        # 初始化游戏，并创建游戏资源
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        # 开始游戏的主循环
+        while True:
+            # 监视键盘和鼠标
+            for enent in pygame.event.get():
+                if enent.type == pygame.QUIT:
+                    sys.exit()
+            # 让最近绘制的屏幕可以看到,刷新
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # 创建游戏实例，并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
