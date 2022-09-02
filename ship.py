c937708d1a4c0437769a8d8027037ddb1121e7e8
yexian <guanpengpeng@yeah.net>
2022-09-02 13:25:44 +0800

    飞船更新

--- ship.py
+++ ship.py
@@ -1,27 +1,29 @@
 import pygame
+
+
 class Ship:
     """管理飞船的类"""
-    def __init__(self,ai_game):
+
+    def __init__(self, ai_game):
         """初始化飞船，并设置初始属性"""
-        self.screen=ai_game.screen
-        self.screen_rect=ai_game.screen.get_rect()
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
         """加载飞船图像，并获取其外接矩形"""
-        self.image=pygame.image.load('images/ship.bmp')
-        self.rect=self.image.get_rect()
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
 
         """对于每艘新飞船，都将其放在屏幕底部的中央"""
-        self.rect.midbottom=self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
         """向右移动标志"""
-        self.moving_right=False
+        self.moving_right = False
+
     def updata(self):
         """根据飞船的移动标志来调整位置"""
         if self.moving_right:
-            self.rect.x+=1
-
+            self.rect.x += 1
 
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image,self.rect)
-
+        self.screen.blit(self.image, self.rect)
