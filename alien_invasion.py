228c1fbcaf243cb8e00f2855cbe15ca416efa4f7
yexian <guanpengpeng@yeah.net>
2022-09-02 10:43:34 +0800

    保存所有设置的类

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,26 +3,33 @@ import pygame
 
 
 class AlienInvasion:
-    # 管理游戏资源和行为的类
+    """管理游戏资源和行为的类"""
+
     def __init__(self):
-        # 初始化游戏，并创建游戏资源
+        """初始化游戏，并创建游戏资源"""
         pygame.init()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        """设置背景颜色"""
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        # 开始游戏的主循环
+        """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标
+            """监视键盘和鼠标"""
             for enent in pygame.event.get():
                 if enent.type == pygame.QUIT:
                     sys.exit()
-            # 让最近绘制的屏幕可以看到,刷新
+
+            """每次循环时颜色重置"""
+            self.screen.fill(self.bg_color)
+
+            """让最近绘制的屏幕可以看到,刷新"""
             pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # 创建游戏实例，并运行游戏
+    """创建游戏实例，并运行游戏"""
     ai = AlienInvasion()
     ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """储存《外星人游戏》所有设置的类"""
+    def __init__(self):
+        """初始化所有设置"""
+        #屏幕设置
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
